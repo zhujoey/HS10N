5eb8c43a56709728ad1927f26123bef3cbf21107
il798li <92280179+il798li@users.noreply.github.com>
2024-12-06 18:48:40 -0800

    added drive code

--- Drive/src/main.cpp
+++ Drive/src/main.cpp
@@ -1,138 +1,117 @@
 #include "main.h"
+#include "lemlib/api.hpp"
+#include <cmath>
 
 pros::MotorGroup driveleft ({-11, -12, -13});
 pros::MotorGroup driveright ({18, 19, 20});
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
+pros::ADIDigitalOut clamp (8);
+pros::Motor ladyBrown (1);
 
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
+class Intake {
+	public: 
+		pros::Motor intake;
+		short intakeSave;
+		short intakeCurrent;
+	
+
+	public:
+		Intake (const int port): intake(port), intakeSave(0), intakeCurrent(0) {}
+
+		void registerClick (const short intakeNew) {
+			this -> intakeSave = this -> intakeCurrent;
+			this -> intakeCurrent = intakeNew;
+		}
+
+		short direction () {
+			if (this -> intakeSave == this -> intakeCurrent) {
+				this -> intakeCurrent = 0;
+			}
+			return this -> intakeCurrent;
+		}
+
+		void run (const float ladyBrownVelocity) {
+			if (ladyBrownVelocity < 0) {
+				this -> intake.move_velocity(-63);
+			} else {
+				const short direction = this -> direction();
+				this -> intake.move_velocity (direction * 127);
+			}
+		}
+};
+
+Intake intake(2);
+pros::IMU imu(3);
+
 void on_center_button()
-{
-
-}
-
-/**
- * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
- */
+{}
 void initialize()
 {
-
 }
 
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
 void disabled()
-{
+{} 
 
-}
-
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
 void competition_initialize()
-{
+{}
+
+
+void autonomous () {
 
 }
 
-void autonomous()
-{
-
-}
-
-void opcontrol()
-{
-	pros::Controller controller(pros::E_CONTROLLER_MASTER);
-	pros::Motor ladyBrown (1);
-	pros::Motor intake(2);
-	pros::adi::DigitalOut clamp (8);
-	int speed = 0;
-	int turning = 0;
-	bool L1pressed = false;
-	bool L2pressed = false;
-	bool clampIn = false;
-	bool moving = false;
-	float ladyBrownVelocity = 0;
-	float intakespeed = 0;
+void opcontrol() {
 	
 	ladyBrown.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+	//pros::Motor intake(2);
+	pros::IMU imu (3);
 
-	while (true)
-	{
-		if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L1))
-		{
-			if (intakespeed == 1)
-			{
-				intakespeed = 0;
-			}
-			else
-			{
-				intakespeed = 1;
-			}
-		}
+	pros::ADIDigitalIn limitSwitch(7);
 
-		if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L2))
-		{
-			if (intakespeed == -1)
-			{
-				intakespeed = 0;
-			}
-			else
-			{
-				intakespeed = -1;
-			}
-		}
+	int speed = 0;
+	int turning = 0;
+	int intakespinforward = 1;
 
-		if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R1))
-		{
-			clampIn = !clampIn;
-		}
+	bool intakeSave = NULL; //true = L1/forward, false = L2/backward
+	bool intakeCurrent = NULL;
 
-		if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_Y))
-		{
-			moving = false;
-			ladyBrownVelocity = -1;
-		}
-		else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT))
-		{
-			moving = false;
-			ladyBrownVelocity = 1;
-		}
-		else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2))
-		{
-			moving = true;
-			intakespeed = 0.5;
-			ladyBrown.move_absolute(30, 100);
-		}
-		
-		if (!moving)
-		{
-			ladyBrownVelocity = 0;
-		}
+	bool clampIn = false;
+	float ladyBrownVelocity = 0;
+	bool loadLadyBrown = false;
 
+	while (true) {
+		// Arcade Drive
 		speed = pow(controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y), 3) / 16129;
 		turning = pow(controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X), 3) / 16129;
 
+		if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L1)) {
+			intake.registerClick(1);
+		}
+		if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L2)) {
+			intake.registerClick(-1);
+		}
+		if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R1)) {
+			clampIn = !clampIn;
+		}
+		if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R2)) {
+			ladyBrown.move_absolute(0, 200);
+		}
+		if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_RIGHT)) {
+			ladyBrown.move_absolute(-120, 200);
+		}
+		if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_Y)) {
+			ladyBrown.move_absolute(-2.5 * 360, 200);
+			intake.intake.move_absolute(-360, 200);
+		}
+
+		intake.run(ladyBrownVelocity);
+		ladyBrownVelocity = 0;
 		clamp.set_value(clampIn);
-		ladyBrown.move_velocity(ladyBrownVelocity * 200);
-		intake.move(127 * intakespeed);
+
+		//intake.move(intakespinforward * 127);
 		driveleft.move(speed + turning);
 		driveright.move(speed - turning);
 		
 		pros::delay(20);
 	}
-};
+}
